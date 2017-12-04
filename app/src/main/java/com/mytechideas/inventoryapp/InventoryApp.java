18c7738eded07a2a399a5de083125cba91daf24b
juandgaines <jdavid90.2@gmail.com>
2017-12-04 11:18:11 -0500

    feat: Project structure
    This is the initial commit of the project.
    
    Resolves:NA
    See also:NA

---
+++ app/src/main/java/com/mytechideas/inventoryapp/InventoryApp.java
@@ -0,0 +1,13 @@
+package com.mytechideas.inventoryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InventoryApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inventory_app);
+    }
+}
