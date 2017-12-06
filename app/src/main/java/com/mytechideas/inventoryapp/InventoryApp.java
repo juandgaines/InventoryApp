db37530889bafedfca7bcc8fc86387623e7f6848
juandgaines <jdavid90.2@gmail.com>
2017-12-06 17:15:36 -0500

    feat:Second activity added
    Activity it's  accesed by intent through the FAB
    
    Resolves:NA
    See also: NA

--- app/src/main/java/com/mytechideas/inventoryapp/InventoryApp.java
+++ app/src/main/java/com/mytechideas/inventoryapp/InventoryApp.java
@@ -1,7 +1,10 @@
 package com.mytechideas.inventoryapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class InventoryApp extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class InventoryApp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inventory_app);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(InventoryApp.this, InsertActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
