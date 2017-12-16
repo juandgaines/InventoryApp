bbf881f6139d987629bf4c7d8c7e9b55dfb55c55
juandgaines <jdavid90.2@gmail.com>
2017-12-16 14:06:37 -0500

    feat: Camera button added with spinner prototype
    We added the camera ImageView in order to take photo of the product. It's just launched camera intent. Also, we prototype the spinners for currency purposes with different providers. We added as well different resources files such as: arrays.xml, colors.xml,dimens.xml ,strings.xml, styles.xml
    
    Resolves: NA
    See also: NA

--- app/src/main/java/com/mytechideas/inventoryapp/InsertActivity.java
+++ app/src/main/java/com/mytechideas/inventoryapp/InsertActivity.java
@@ -1,13 +1,70 @@
 package com.mytechideas.inventoryapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class InsertActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private ImageView mPhotoButton;
+    private Spinner mCurrencySpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+        mCurrencySpinner = (Spinner) findViewById(R.id.spinner_currency);
+        mPhotoButton= (ImageView) findViewById(R.id.image_button);
+
+        mPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+    }
+
+    private void setupSpinner() {
+        // Create adapter for spinner. The list options are from the String array it will use
+        // the spinner will use the default layout
+        ArrayAdapter genderSpinnerAdapter = ArrayAdapter.createFromResource(this,
+                R.array.array_gender_options, android.R.layout.simple_spinner_item);
+
+        // Specify dropdown layout style - simple list view with 1 item per line
+        genderSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+
+        // Apply the adapter to the spinner
+        mCurrencySpinner.setAdapter(genderSpinnerAdapter);
+
+        // Set the integer mSelected to the constant values
+        /*mCurrencySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selection = (String) parent.getItemAtPosition(position);
+                if (!TextUtils.isEmpty(selection)) {
+                    if (selection.equals(getString(R.string.currency_colombia))) {
+                        mGender = PetEntry.GENDER_MALE; // Male
+                    } else if (selection.equals(getString(R.string.currency_usa))) {
+                        mGender = PetEntry.GENDER_FEMALE; // Female
+                    }
+                }
+            }
+
+            // Because AdapterView is an abstract class, onNothingSelected must be defined
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                mGender = PetEntry.GENDER_UNKNOW; // Unknown
+            }
+        });
+        */
     }
 }
